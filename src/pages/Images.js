902d7925085dbd6ccbb3da2f26978778fa247ab3
mbayer2303 <mbayer2303@gmail.com>
2021-01-18 14:19:04 +0100

    Added editor for images

--- src/pages/Images.js
+++ src/pages/Images.js
@@ -1,4 +1,7 @@
+import React from "react";
+import Row from "../components/Responsive/Row";
 import makeDataPage from "../helpers/makeDataPage";
+import TextField from "../components/Material/TextField";
 
 const columns = [
   {
@@ -22,10 +25,40 @@ const primaryProperty = {
   display: "url",
 };
 
+const ImageEditor = ({ data, onUpdate }) => {
+  return (
+    <React.Fragment>
+      <Row>
+        <TextField
+          label="Url"
+          onChange={(e) => onUpdate("url", e.target.value)}
+          value={data.url}
+        />
+        <TextField
+          label="Alternate text"
+          onChange={(e) => onUpdate("alt", e.target.value)}
+          value={data.alt}
+        />
+        <TextField
+          label="Width"
+          onChange={(e) => onUpdate("width", e.target.value)}
+          value={data.width}
+        />
+        <TextField
+          label="Height"
+          onChange={(e) => onUpdate("height", e.target.value)}
+          value={data.height}
+        />
+      </Row>
+    </React.Fragment>
+  );
+};
+
 export default makeDataPage({
   pageHeader: "Images",
   columns,
   primaryProperty,
   entity: "images",
   endpoint: "images",
+  Editor: ImageEditor,
 });
