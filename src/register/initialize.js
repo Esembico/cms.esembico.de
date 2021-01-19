a73496bfed168be38ffbb59701bda11b793b99f4
mbayer2303 <mbayer2303@gmail.com>
2021-01-19 19:21:39 +0100

    Created page options

--- src/register/initialize.js
+++ src/register/initialize.js
@@ -3,8 +3,10 @@ import teamOptions from "./options/teamOptions";
 import recommendationOptions from "./options/recommendationOptions";
 import imageOptions from "./options/imageOptions";
 import prototypeOptions from "./options/prototypeOptions";
+import pageOptions from "./options/pageOptions";
 
 StateRegister.register("team", teamOptions);
 StateRegister.register("recommendations", recommendationOptions);
 StateRegister.register("images", imageOptions);
 StateRegister.register("prototypes", prototypeOptions);
+StateRegister.register("pages", pageOptions);
---
+++ src/register/options/pageOptions.js
@@ -0,0 +1,18 @@
+const pageOptions = {
+  columns: [
+    {
+      header: "Name",
+      display: "name",
+    },
+    {
+      header: "Slug",
+      display: "slug",
+    },
+  ],
+  primaryProperty: {
+    header: "Name",
+    display: "name",
+  },
+};
+
+export default pageOptions;
