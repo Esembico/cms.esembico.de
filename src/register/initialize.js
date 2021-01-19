ae138aa665198121e852c5b27c18bd783629bf54
mbayer2303 <mbayer2303@gmail.com>
2021-01-19 19:20:00 +0100

    Created prototypes options

--- src/register/initialize.js
+++ src/register/initialize.js
@@ -2,7 +2,9 @@ import StateRegister from "./StateRegister";
 import teamOptions from "./options/teamOptions";
 import recommendationOptions from "./options/recommendationOptions";
 import imageOptions from "./options/imageOptions";
+import prototypeOptions from "./options/prototypeOptions";
 
 StateRegister.register("team", teamOptions);
 StateRegister.register("recommendations", recommendationOptions);
 StateRegister.register("images", imageOptions);
+StateRegister.register("prototypes", prototypeOptions);
---
+++ src/register/options/prototypeOptions.js
@@ -0,0 +1,18 @@
+const prototypeOptions = {
+  columns: [
+    {
+      header: "Title",
+      display: "title",
+    },
+    {
+      header: "Subtitle",
+      display: "subtitle",
+    },
+  ],
+  primaryProperty: {
+    header: "Title",
+    display: "title",
+  },
+};
+
+export default prototypeOptions;
