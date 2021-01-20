f29e5c9ae9adc76521f714dd9f16218e39fdc81b
mbayer2303 <mbayer2303@gmail.com>
2021-01-20 16:40:44 +0100

    Added editor for prototypes

--- src/register/options/prototypeOptions.js
+++ src/register/options/prototypeOptions.js
@@ -13,6 +13,47 @@ const prototypeOptions = {
     header: "Title",
     display: "title",
   },
+  editor: [
+    {
+      type: "text",
+      label: "Title",
+      name: "title",
+    },
+    {
+      type: "text",
+      label: "Subtitle",
+      name: "subtitle",
+    },
+    {
+      type: "text",
+      label: "Url",
+      name: "url",
+    },
+    {
+      type: "reference",
+      label: "Image",
+      name: "image",
+      to: "images",
+    },
+    {
+      type: "text",
+      label: "Challenge",
+      name: "challenge",
+      multiline: true,
+    },
+    {
+      type: "text",
+      label: "Solution",
+      name: "solution",
+      multiline: true,
+    },
+    {
+      type: "text",
+      label: "Rough details",
+      name: "rough_details",
+      multiline: true,
+    },
+  ],
 };
 
 export default prototypeOptions;
