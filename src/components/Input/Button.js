fed869b4672df24dbcda4cbb61823085d43669f7
mbayer2303 <mbayer2303@gmail.com>
2021-01-23 17:46:57 +0100

    Added different button variations

--- src/components/Input/Button.js
+++ src/components/Input/Button.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-export default function Button({ children, ...other }) {
+export default function Button({ children, type, ...other }) {
   return (
     <div className="button-container">
-      <button type="button" {...other}>
+      <button type="button" className={type || "primary"} {...other}>
         {children}
       </button>
     </div>
