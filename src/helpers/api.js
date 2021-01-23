66c99e864ce0aba1a6a469608155a6d913f97cc8
mbayer2303 <mbayer2303@gmail.com>
2021-01-23 11:18:31 +0100

    Added a fetchWrapper that rejects if res.ok is false

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -8,3 +8,21 @@ export function generateHeaders(token) {
 
   return headers;
 }
+
+export function fetchWrapper(url, options) {
+  return new Promise((resolve, reject) => {
+    fetch(url, options)
+      .then((res) => {
+        if (!res.ok) {
+          res.json().then((json) => {
+            reject({ status: res.status, message: json });
+          });
+          return;
+        }
+        res.json().then((json) => {
+          resolve(json);
+        });
+      })
+      .catch(reject);
+  });
+}
