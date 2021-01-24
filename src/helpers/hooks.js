43c1efa9d8ac807509b569a8e221c14e2640b457
mbayer2303 <mbayer2303@gmail.com>
2021-01-24 19:43:47 +0100

    Added an useOutsideAlerter hook.
    The hook calls a callback when the user clicks outside of the
    specified element

---
+++ src/helpers/hooks.js
@@ -0,0 +1,16 @@
+import { useEffect } from 'react';
+
+export function useOutsideAlerter(ref, callback) {
+  useEffect(() => {
+    function handleClickOutside(e) {
+      if (ref.current && !ref.current.contains(e.target)) {
+        callback();
+      }
+    }
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [ref]);
+}
