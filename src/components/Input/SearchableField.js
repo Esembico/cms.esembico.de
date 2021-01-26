a746f4f7c1adafea5c66c0aeb7b044cd69d8ed21
mbayer2303 <mbayer2303@gmail.com>
2021-01-26 12:52:50 +0100

    Changed the implementation of the SearchableField

--- src/components/Input/SearchableField.js
+++ src/components/Input/SearchableField.js
@@ -1,8 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import { TextField } from '@material-ui/core';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import React from 'react';
 import { useDispatch, useStore } from 'react-redux';
 import getDisplayValue from '../../helpers/getDisplayValue';
 import stateRegister from '../../register/stateRegister';
-import TextField from './TextFieldImpl';
 
 export default function SearchableField({
   label,
@@ -11,9 +12,6 @@ export default function SearchableField({
   onChange,
   errors
 }) {
-  const [internalValue, setInternalValue] = useState(value);
-  const [dropdownOpen, setDropdownOpen] = useState(false);
-  const [selectedIndex, setSelectedIndex] = useState(-1);
   const state = useStore().getState();
   const setFilteredDataAction = stateRegister.getAction(
     entity,
@@ -30,82 +28,33 @@ export default function SearchableField({
   const primaryProperty = stateRegister.getOption(entity, 'primaryProperty');
   const dispatch = useDispatch();
 
-  useEffect(() => {
-    setInternalValue(value);
-  }, [value]);
-
-  const onInputChange = (e) => {
-    const value = e.target.value;
-    setDropdownOpen(!!value);
-    setInternalValue(value);
+  const onInputChange = (e, value) => {
     dispatch(setFilteredDataAction(value));
-    if (!value) {
-      onChange(null);
-    }
-  };
-
-  const changeSelection = (entry) => {
-    setDropdownOpen(false);
-    onChange(entry);
-  };
-
-  const onKeyDown = (e) => {
-    let newIndex = selectedIndex;
-
-    if (e.keyCode === 40) {
-      newIndex++;
-    } else if (e.keyCode === 38) {
-      newIndex--;
-    } else if (e.keyCode === 13) {
-      changeSelection(data[selectedIndex]);
-    }
-
-    if (newIndex !== selectedIndex) {
-      if (newIndex < 0) {
-        newIndex = 0;
-      } else if (newIndex >= data.length) {
-        newIndex = data.length - 1;
-      }
-      setInternalValue(data[newIndex]);
-      setSelectedIndex(newIndex);
-    }
-  };
-
-  const onBlur = () => {
-    setDropdownOpen(false);
   };
 
   return (
     <React.Fragment>
-      <div className='searchable-container'>
-        <TextField
-          label={label}
-          value={getDisplayValue(internalValue, primaryProperty.display)}
-          onChange={onInputChange}
-          onKeyDown={onKeyDown}
-          onBlur={onBlur}
-          errors={errors}
-        />
-        {dropdownOpen && (
-          <div className='searchable-field-dropdown'>
-            {data.map((entry, i) => {
-              return (
-                <a
-                  onClick={(e) => {
-                    e.preventDefault();
-                    changeSelection(entry);
-                  }}
-                  className={i === selectedIndex ? 'active' : ''}
-                  key={entry.id}
-                  href={`#entry-${entry.id}`}
-                >
-                  {getDisplayValue(entry, primaryProperty.display)}
-                </a>
-              );
-            })}
-          </div>
+      <Autocomplete
+        options={data}
+        getOptionLabel={(option) =>
+          getDisplayValue(option, primaryProperty.display)
+        }
+        onChange={(_, newValue) => {
+          onChange(newValue);
+        }}
+        onInputChange={onInputChange}
+        value={value}
+        renderInput={(params) => (
+          <TextField
+            error={!!errors}
+            helperText={errors?.join('\n')}
+            {...params}
+            label={label}
+            margin='normal'
+          />
         )}
-      </div>
+        getOptionSelected={(option, value) => option.id === value.id}
+      />
     </React.Fragment>
   );
 }
