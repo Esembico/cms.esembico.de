652d8e31e1d1a685b34376fae7f7857e4cfebbe3
mbayer2303 <mbayer2303@gmail.com>
2021-02-01 19:06:53 +0100

    Replaced any with ReduxStore in alert selectors

--- src/redux/reducers/selectors/alert.ts
+++ src/redux/reducers/selectors/alert.ts
@@ -1,16 +1,16 @@
 import { AlertData, AlertState } from '../types/alert';
+import { ReduxStore } from '../types/base';
 
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-export const getState = (store: any): AlertState => {
+export const getState = (store: ReduxStore): AlertState => {
   return store.alert;
 };
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-export const getAlerts = (store: any): Record<string, AlertData> => {
+export const getAlerts = (store: ReduxStore): Record<string, AlertData> => {
   return getState(store).alerts;
 };
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-export const getAlertList = (store: any): Array<AlertData> => {
+export const getAlertList = (store: ReduxStore): Array<AlertData> => {
   return Object.values(getAlerts(store));
 };
